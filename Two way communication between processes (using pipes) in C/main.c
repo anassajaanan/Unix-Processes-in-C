af9b147f1f29d1a9456c23f130c6572a8c4cbe7c
Anas Ajaanan <aajaanan@lab1r5s4.42abudhabi.ae>
2023-08-05 19:40:40 +0400

    Two way communication between processes

--- Two way communication between processes (using pipes) in C/main.c
+++ Two way communication between processes (using pipes) in C/main.c
@@ -3,13 +3,20 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <time.h>
 
 
 int main(void)
 {
-	int fd[2];
+	int fd1[2]; // parent will write to it and child will read from it
+	int fd2[2]; // parent will read from it and child will write to it
 
-	if (pipe(fd) == -1)
+	if (pipe(fd1) == -1)
+	{
+		perror("pipe");
+		return (EXIT_FAILURE);
+	}
+	if (pipe(fd2) == -1)
 	{
 		perror("pipe");
 		return (EXIT_FAILURE);
@@ -23,53 +30,56 @@ int main(void)
 	else if (pid == 0)
 	{
 		// child process
+		close(fd1[1]);
+		close(fd2[0]);
 		int num;
 		for (int i = 0; i < 5; i++)
 		{
-			if (read(fd[0], &num, sizeof(int)) == -1)
+			if (read(fd1[0], &num, sizeof(int)) == -1)
 			{
 				perror("read");
 				return (EXIT_FAILURE);
 			}
 
-			printf("Child Recieving ... %d\n", num);
+			// printf("Child Recieving ... %d\n", num);
 			num = num * 4;
-			if (write(fd[1], &num, sizeof(int)) == -1)
+			if (write(fd2[1], &num, sizeof(int)) == -1)
 			{
 				perror("write");
 				return (EXIT_FAILURE);
 			}
-			printf("Child Sending ... %d\n", num);
+			// printf("Child Sending ... %d\n", num);
 		}
-		close(fd[0]);
-		close(fd[1]);
+		close(fd1[0]);
+		close(fd2[1]);
 	}
 	else
 	{
 		//parent process
+		close(fd1[0]);
+		close(fd2[1]);
 		for (int i = 0; i < 5; i++)
 		{
 			int num = rand() % 20;
-			if (write(fd[1], &num, sizeof(int)) == -1)
+			if (write(fd1[1], &num, sizeof(int)) == -1)
 			{
 				perror("write");
 				return (EXIT_FAILURE);
 			}
 			printf("Parent Sending ... %d\n", num);
 		}
-		wait(NULL);
+		close(fd1[1]);
 		for (int i = 0; i < 5; i++)
 		{
 			int new_num;
-			if (read(fd[0], &new_num, sizeof(int)) == -1)
+			if (read(fd2[0], &new_num, sizeof(int)) == -1)
 			{
 				perror("read");
 				return (EXIT_FAILURE);
 			}
 			printf("Parent Receieving ... %d\n", new_num);
 		}
-		close(fd[1]);
-		close(fd[0]);
+		close(fd2[0]);
 	}
 	return (0);
 }
